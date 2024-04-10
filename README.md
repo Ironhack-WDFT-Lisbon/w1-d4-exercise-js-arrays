6b3adfee74f2afa9fd671ac069ab9a2aed44afb0
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-10 23:56:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# w1-d4-exercise-js-arrays
+
+1. Refactor the raceResults function to an arrow function
+2. Perform the following operations on the students array:
+   1. A new student joined the classes, add him/her to the array
+   2. Peter quit classes remove him from the array
+3. Create a function that returns the number of words in a string. If you want, use the value of content to test it.
---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Ready to go! 🚀");
+
+// ------------------------------
+// Iteration 1
+// ------------------------------
+// const raceResults = ["Helen", "John", "Peter", "Merry"];
+// raceResults.forEach(function (elem, index) {
+//   console.log(elem + " finished the race in " + (index + 1) + " position!");
+// });
+
+// ------------------------------
+// Iteration 2
+// ------------------------------
+const students = ["John", "Peter", "Merry"];
+
+// ------------------------------
+// Iteration 3
+// ------------------------------
+const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
