bb8c33c742a9741f97eb0ed6c18b402818507277
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-07-11 08:18:39 +0100

    update readme

--- script.js
+++ script.js
@@ -4,36 +4,17 @@ console.log("Ready to go! 🚀");
 // Iteration 1
 // ------------------------------
 const raceResults = ["Helen", "John", "Peter", "Merry"];
+
 // raceResults.forEach(function (elem, index) {
 //   console.log(elem + " finished the race in " + (index + 1) + " position!");
 // });
-
-// raceResults.forEach((elem, index) =>
-//   console.log(elem + " finished the race in " + (index + 1) + " position!")
-// );
 
 // ------------------------------
 // Iteration 2
 // ------------------------------
 const students = ["John", "Peter", "Merry"];
 
-// Add a new student to the array
-// students.push("Helen");
-// console.log(students);
-
-// Remove Peter from the array
-// const index = students.indexOf("Peter");
-// students.splice(index, 1);
-// console.log(students);
-
 // ------------------------------
 // Iteration 3
 // ------------------------------
 const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-
-// Create a function that counts the words in a string
-function countWords(string) {
-  return string.split(" ").length;
-}
-
-// console.log(countWords(content));
